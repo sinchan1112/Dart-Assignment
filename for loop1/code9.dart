af914257e6cad5fd66076e9db69316fde614fd11
vishvajeetchopade <152198104+vishvajeetchopade@users.noreply.github.com>
2024-09-12 22:41:44 +0530

    Add files via upload

---
+++ for loop1/code9.dart
@@ -0,0 +1,7 @@
+void main() {
+  int x = 0;
+  for (int i = 1; i <= 10; i++) {
+    x += i;
+  }
+  print(x);
+}
---
+++ nested-for-loop/code4.dart
@@ -0,0 +1,15 @@
+import"dart:io";
+void main(){
+  print("enter rows : ");
+  int rows = int.parse(stdin.readLineSync()!);
+  int val = (rows*(rows+1))~/2;
+  for(int i=1;i<=rows;i++){
+    for(int j=1;j<=i;j++){
+      stdout.write("$val");
+      if(j!=1){
+        val--;
+      }
+    }
+    print(" ");
+  }
+}
