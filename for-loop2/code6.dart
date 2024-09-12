bd67f1dfeecc22f6c2f044708a7f7c969fda9e7d
vishvajeetchopade <152198104+vishvajeetchopade@users.noreply.github.com>
2024-09-12 22:57:13 +0530

    Add files via upload

---
+++ for-loop2/code6.dart
@@ -0,0 +1,12 @@
+import"dart:io";
+void main(){
+  int start = 20;
+  int end = 10;
+  while(start>=end){
+    if(start%2==1){
+      stdout.write(start*start);
+      stdout.write(" ")
+    }
+    start--;
+  }
+}
